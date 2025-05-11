2c5a5abd55b5a4174405d122b757107bdcc4685a
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-05-12 01:31:23 +0600

    first commit

---
+++ Switch.js
@@ -0,0 +1,13 @@
+const day = "sun";
+
+switch (day) {
+    case "sat":
+        console.log("5am");
+        break;
+    case "sun":
+        console.log("9am");
+        break;
+    default:
+        console.log("7pm");
+        
+}
