555577430797bcc6856c3d82215ac1158a8cbf8c
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-05-13 10:11:58 +0600

    fasd

---
+++ constac.js
@@ -0,0 +1,7 @@
+function Data(num, sum) {
+  this.num = num;
+  this.sum = sum;
+}
+
+let code = new Data("rifat", 54);
+console.log(code);
---
+++ for.js
@@ -0,0 +1,10 @@
+const data = {
+    name : "rifat",
+    age : 54,
+    person: "Male"
+}
+
+for( let valu in data){
+    console.log(valu.valueOf);
+    
+}
