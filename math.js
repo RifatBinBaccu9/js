cb56e5b807e1efa87729c6931bfb84bb3bcf0989
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-05-15 01:15:37 +0600

    math

---
+++ math.js
@@ -0,0 +1,9 @@
+console.log(Math);
+console.log(Math.abs(-4.5)); /* negitive value posetive revers kora */
+console.log(Math.round(5.7)); /* top value set */
+console.log(Math.ceil(4.1)); /* over then top value set */
+console.log(Math.floor(4.8)); /* over value then less value caseing */
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
